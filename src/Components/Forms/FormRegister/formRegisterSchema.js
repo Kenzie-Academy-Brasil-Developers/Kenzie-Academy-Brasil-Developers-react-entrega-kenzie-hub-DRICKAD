8381b61a4f40dc7cdbae81049ea365659252f88b
Adriana Silva Domingos <106705801+DrickaD@users.noreply.github.com>
2024-01-08 16:04:18 -0300

    correção na validação de caractere especial
    tinha esquecido de corrigir a validação de caractere especial

--- src/Components/Forms/FormRegister/formRegisterSchema.js
+++ src/Components/Forms/FormRegister/formRegisterSchema.js
@@ -11,7 +11,9 @@ export const FormRegisterSchema = z
         .min(8, "É necessário pelo menos oito caracteres.")
         .regex(/(?=.*?[A-Z])/, "É necessário pelo menos uma letra maiúscula")
         .regex(/(?=.*?[a-z])/, "É necessário pelo menos uma letra minúscula")
-        .regex(/(?=.*?[0-9])/, "É necessário pelo menos um número."),
+        .regex(/(?=.*?[0-9])/, "É necessário pelo menos um número.")
+        .regex(/(?=.*?[@}{,.^?~=+\-_\/*\-+.\|])/, "É necessário pelo menos um caractere especial."),
+
 
     confirmPassword: z.string("campo obrigatório").min(1, "campo obrigatório"),
     bio:z.string("campo obrigatório").min(1, "campo obrigatório"),
